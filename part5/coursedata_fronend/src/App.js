9b564237ef7eb2b9162d96f360ae23947a3bd8a0
Max Rantil <rantil@pm.me>
2023-06-03 15:25:31 +0300

    part5 coursedata start

---
+++ part5/coursedata_fronend/src/App.js
@@ -0,0 +1,193 @@
+import { useState, useEffect } from "react"
+import loginService from './services/login'
+import Note from './components/Note'
+import noteService from './services/notes'
+import Notification from "./components/Notification"
+import Footer from "./components/Footer"
+
+const App = () => {
+  const [notes, setNotes] = useState(null)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('')
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  const hook = () => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        console.log('InitialNotes:', initialNotes);
+        setNotes(initialNotes)
+      })
+  }
+
+  useEffect(hook, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      window.localStorage.setItem(
+        'loggedNoteappUser', JSON.stringify(user)
+      )
+      noteService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+
+    if (!notes) {
+      return null
+    }
+
+    const toggleImportanceOf = id => {
+      const note = notes.find(n => n.id === id)
+      const changedNote = { ...note, important: !note.important }
+
+      noteService
+        .update(id, changedNote).then(returnedNote => {
+          setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+        })
+        .catch(error => {
+          setErrorMessage(
+            `Note '${note.content}' was already removed from server`
+          )
+          setTimeout(() => {
+            setErrorMessage(null)
+          }, 5000)
+          setNotes(notes.filter(n => n.id !== id))
+        })
+    }
+
+    const handleNoteChange = (event) => {
+      console.log(event.target.value)
+      setNewNote(event.target.value)
+    }
+
+    const addNote = (event) => {
+      event.preventDefault()
+      const noteObject = {
+        content: newNote,
+        important: Math.random() < 0.5,
+        id: notes.length + 1,
+      }
+
+      noteService
+        .create(noteObject)
+        .then(returnedNote => {
+          console.log(returnedNote)
+          setNotes(notes.concat(returnedNote))
+          setNewNote('')
+        })
+    }
+
+    const notesToShow = showAll
+      ? notes
+      : notes.filter(note => note.important)
+
+    const loginForm = () => (
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    )
+
+    const noteForm = () => (
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
+    )
+
+    return (
+      <div>
+        <h1>Notes</h1>
+        <Notification message={errorMessage} />
+
+        {!user && loginForm()}
+        {user && <div>
+          <p>{user.name} logged in</p>
+          {noteForm()}
+        </div>
+        }
+
+        <h2>Login</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+
+        <div>
+          <button onClick={() => setShowAll(!showAll)}>
+            show {showAll ? 'important' : 'all'}
+          </button>      </div>
+        <ul>
+          {notesToShow.map(note =>
+            <Note
+              key={note.id}
+              note={note}
+              toggleImportance={() => toggleImportanceOf(note.id)}
+            />
+          )}
+        </ul>
+        <form onSubmit={addNote}>
+          <input
+            value={newNote}
+            onChange={handleNoteChange}
+          />
+          <button type="submit">save</button>
+        </form>
+        <Footer />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ part5/coursedata_fronend/src/services/notes.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+const baseUrl = '/api/notes'
+
+const getAll = () => {
+	const request = axios.get(baseUrl)
+	return request.then(response => {
+		return response.data
+	})
+}
+
+const create = newObject => {
+	const request = axios.post(baseUrl, newObject)
+	return request.then(response => {
+		return response.data
+	})
+}
+
+const update = (id, newObject) => {
+	const request = axios.put(`${baseUrl}/${id}`, newObject)
+	return request.then(response => {
+		return response.data
+	})
+}
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+	getAll,
+	create,
+	update
+}
