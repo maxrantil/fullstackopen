7e9613878eccd1bcbf5621716404f6dfc63e6cf5
Max Rantil <rantil@pm.me>
2023-06-12 09:59:37 +0300

    5.14: Blog list tests, step2

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render, screen } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from '../components/Blog'
 
 describe('<Blog />', () => {
@@ -19,18 +19,34 @@ describe('<Blog />', () => {
     username: 'testuser'
   }
 
+  let component
+
   beforeEach(() => {
-    render(<Blog blog={blog} user={user} />)
+    component = render(<Blog blog={blog} user={user} />)
   })
 
   test('renders title and author, but not url or likes by default', () => {
-    expect(screen.getByText('Test blog')).toBeDefined()
-    expect(screen.getByText('Test author')).toBeDefined()
+    expect(component.getByText('Test blog')).toBeDefined()
+    expect(component.getByText('Test author')).toBeDefined()
 
-    const urlElement = screen.queryByText('http://test.com')
+    const urlElement = component.queryByText('http://test.com')
     expect(urlElement).toBeNull()
 
-    const likesElement = screen.queryByText('likes 5')
+    const likesElement = component.queryByText('likes 5')
     expect(likesElement).toBeNull()
+  })
+
+  test('renders url and likes when view button is clicked', () => {
+    const button = component.getByText('view')
+    fireEvent.click(button)
+
+    const urlElement = component.getByText('http://test.com')
+    expect(urlElement).toBeDefined()
+
+    const likesLabel = component.getByText('likes')
+    expect(likesLabel).toBeDefined()
+
+    const likesCount = component.getByText('5')
+    expect(likesCount).toBeDefined()
   })
 })
