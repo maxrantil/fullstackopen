01959af5edc07b1cabefb75878afd9d44a5a2913
Max Rantil <rantil@pm.me>
2023-06-15 14:10:44 +0300

    part 5 final commit

--- part5/bloglist-frontend/cypress.config.js
+++ part5/bloglist-frontend/cypress.config.js
@@ -1,13 +1,13 @@
+/* eslint-env node */
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents() {
     },
     baseUrl: 'http://localhost:3000',
   },
   env: {
-
     BACKEND: 'http://localhost:3001/api'
   }
 })
