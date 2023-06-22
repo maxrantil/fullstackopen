dbd0efe157147de09c19bcbed3247b77a78658c6
Max Rantil <rantil@pm.me>
2023-06-22 09:52:03 +0300

    removed comment

--- part2/phonebook/src/components/DeleteButton.js
+++ part2/phonebook/src/components/DeleteButton.js
@@ -1,21 +1,3 @@
-// import personService from '../services/people';
-
-// const DeleteButton = ({ person }) => {
-// 	const handleDelete = () => {
-// 		if (window.confirm(`Delete ${person.name}?`)) {
-// 			personService
-// 				.deletePerson(person.id);
-// 		}
-// 	}
-// 	return (
-// 		<button onClick={handleDelete}>delete</button>
-// 	)
-// }
-
-// export default DeleteButton;
-
-// import personService from '../services/people';
-
 const DeleteButton = ({ person, handleDelete }) => {
     const handleDeleteClick = () => {
         if (window.confirm(`Delete ${person.name}?`)) {
